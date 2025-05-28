0b61b7498729116f0c719c880de539e2f4fbcabd
angelsituu <110509049+angelsituu@users.noreply.github.com>
2025-05-28 23:12:20 +0200

    tdfyeryadyf

--- js/config.js
+++ js/config.js
@@ -50,17 +50,17 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/laorden(1).jpg",
-	"backgrounds/images/laorden(2).jpg",
-	"backgrounds/images/laorden(3).jpg",
-	"backgrounds/images/laorden(4).jpg",
-	"backgrounds/images/laorden(5).jpg",
-	"backgrounds/images/laorden(6).jpg",
-	"backgrounds/images/laorden(7).jpg",
-	"backgrounds/images/laorden(8).jpg",
-	"backgrounds/images/laorden(9).jpg",
-	"backgrounds/images/laorden(10).jpg",
-	"backgrounds/images/laorden(11).jpg"
+	"backgrounds/images/laorden (1).jpg",
+	"backgrounds/images/laorden (2).jpg",
+	"backgrounds/images/laorden (3).jpg",
+	"backgrounds/images/laorden (4).jpg",
+	"backgrounds/images/laorden (5).jpg",
+	"backgrounds/images/laorden (6).jpg",
+	"backgrounds/images/laorden (7).jpg",
+	"backgrounds/images/laorden (8).jpg",
+	"backgrounds/images/laorden (9).jpg",
+	"backgrounds/images/laorden (10).jpg",
+	"backgrounds/images/laorden (11).jpg"
 ];
 
 /*Random background images order?*/
